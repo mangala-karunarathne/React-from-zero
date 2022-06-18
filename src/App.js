5128a472eac07479328d52d06e66ef6c12cad758
mangala-karunarathne <mangalaicc@gmail.com>
2022-06-18 12:24:35 -0700

    Functional Componenets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './Componenets/Greet'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello Mangala....!!!      Welcome to the React World....!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Greet/>
     </div>
   );
 }
