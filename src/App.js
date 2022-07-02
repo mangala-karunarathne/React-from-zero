088c986bb51ee76f423d5a9fffa41ae056194dcc
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-01 23:03:54 -0700

    Bulb - useState - React Hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Greet from './Componenets/Greet'
+import LightBulb from './Componenets/bulb'
 
 function App() {
   return (
     <div className="App">
      <Greet/>
+     <LightBulb/>
     </div>
   );
 }
