363296e98edf1f9b953109b67ae77629289ed7cc
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-01 23:48:22 -0700

    Add Routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import Greet from './Componenets/Greet'
 import LightBulb from './Componenets/bulb'
 import Counter from './Componenets/counter';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     <Greet/>
-     <LightBulb/>
-     <Counter/>
+       
+    <Router>
+    <div>
+          
+      <Routes>
+        <Route exact path='/' element={<Greet/>}/>
+        <Route exact path='/bulb' element={<LightBulb/>}/>
+        <Route exact path='/counter' element={<Counter/>}/>
+      </Routes>
+     
     </div>
+    </Router>
   );
 }
 
