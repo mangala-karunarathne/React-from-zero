aeccf4e3abb0f7ee5a6284675d9e3dcaebad7666
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-03 14:44:25 -0700

    PropDrilling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import LightBulb from './Componenets/bulb';
 import Counter from './Componenets/counter';
 import StateTutorial from './Hooks/useState/useState';
 import EffectTutorial from './Hooks/useEffect/useEffect';
+import Component1 from './Hooks/useContext/PropDrilling/without-use-context';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
         <Route exact path='/counter' element={<Counter/>}/>
         <Route exact path='/usestate' element={<StateTutorial/>}/>
         <Route exact path='/useeffect' element={<EffectTutorial/>}/>
+        <Route exact path='/propdrilling/without' element={<Component1/>}/>
+        <Route exact path='/propdrilling/with' element={<EffectTutorial/>}/>
         
       </Routes>
      
